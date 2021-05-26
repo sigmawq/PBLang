2b76d2c99a8d8a793134be5aa280757543bc70a9
FoxFurry <foxfurry>
2021-05-26 12:52:15 +0300

    Added expression to IR converter

--- source/llvm_ir.h
+++ source/llvm_ir.h
@@ -26,58 +26,55 @@ static std::unique_ptr<llvm::Module> llvm_module;
 static std::unique_ptr<llvm::IRBuilder<>> llvm_builder;
 static std::map<std::string, llvm::Value *> named_values;
 
-void initialize_ir(){
-    llvm_context  = std::make_unique<llvm::LLVMContext>();
-    llvm_module   = std::make_unique<llvm::Module>(JIT_MODULE_NAME, *llvm_context);
-    llvm_builder  = std::make_unique<llvm::IRBuilder<>>(*llvm_context);
+void initialize_ir() {
+    llvm_context = std::make_unique<llvm::LLVMContext>();
+    llvm_module = std::make_unique<llvm::Module>(JIT_MODULE_NAME, *llvm_context);
+    llvm_builder = std::make_unique<llvm::IRBuilder<>>(*llvm_context);
 }
 
 
-llvm::Type* ir_str_to_type(std::string str_type){
-    if(str_type == "int")   return llvm::Type::getInt64Ty(*llvm_context);
-    if(str_type == "float") return llvm::Type::getFloatTy(*llvm_context);
-    if(str_type == "double")return llvm::Type::getDoubleTy(*llvm_context);
-    if(str_type == "bool")  return llvm::Type::getInt1Ty(*llvm_context);
-    if(str_type == "char")  return llvm::Type::getInt1Ty(*llvm_context);
+llvm::Type *ir_str_to_type(std::string str_type) {
+    if (str_type == "int") return llvm::Type::getInt64Ty(*llvm_context);
+    if (str_type == "float") return llvm::Type::getFloatTy(*llvm_context);
+    if (str_type == "double")return llvm::Type::getDoubleTy(*llvm_context);
+    if (str_type == "bool") return llvm::Type::getInt1Ty(*llvm_context);
+    if (str_type == "char") return llvm::Type::getInt1Ty(*llvm_context);
 
     return nullptr;
 }
 
 
-std::shared_ptr<ast_node> get_child(std::shared_ptr<ast_node> &node, AST_NODE_TYPE search_type){
-    for(auto child: node->children) if (child->node_type == search_type) return child; return nullptr;
+std::shared_ptr<ast_node> get_child(std::shared_ptr<ast_node> &node, AST_NODE_TYPE search_type) {
+    for (auto child: node->children) if (child->node_type == search_type) return child;
+    return nullptr;
 }
 
-std::vector<llvm::Type*> get_func_args_types(std::shared_ptr<ast_node> &func_node){
-    std::vector<llvm::Type*> result;
+std::vector<llvm::Type *> get_func_args_types(std::shared_ptr<ast_node> &func_node) {
+    std::vector<llvm::Type *> result;
     int arguments = 0;
-    if (get_child(func_node, F_INPUT_VOID) == nullptr){
-        for(auto f_input: get_child(func_node, F_INPUT)->children){
+    if (get_child(func_node, F_INPUT_VOID) == nullptr) {
+        for (auto f_input: get_child(func_node, F_INPUT)->children) {
             arguments++;
-            if(auto signature = get_child(f_input, VARIABLE_SIGNATURE)){
-                llvm::Type* signature_type = ir_str_to_type(get_child(signature,TYPE_SPEC)->optional_value->value);
-                if(signature_type != nullptr){
+            if (auto signature = get_child(f_input, VARIABLE_SIGNATURE)) {
+                llvm::Type *signature_type = ir_str_to_type(get_child(signature, TYPE_SPEC)->optional_value->value);
+                if (signature_type != nullptr) {
                     result.push_back(signature_type);
                 }
             }
         }
-    }
-    else{
+    } else {
         result.push_back(llvm::Type::getVoidTy(*llvm_context));
     }
     return result;
 }
 
-llvm::Function * func_to_ir(std::shared_ptr<ast_node> &func_node){
-    llvm::Type *return_type;
-    if(get_child(func_node, F_OUTPUT_VOID) != nullptr){
-        return_type = llvm::Type::getVoidTy(*llvm_context);
-    }
-    else{
+llvm::Function *func_to_ir(std::shared_ptr<ast_node> &func_node) {
+    llvm::Type *return_type = llvm::Type::getVoidTy(*llvm_context);;
+    if (get_child(func_node, F_OUTPUT_VOID) == nullptr) {
         auto f_output = get_child(func_node, F_OUTPUT);
-        if(auto signature = get_child(f_output,VARIABLE_SIGNATURE)){
-            llvm::Type* signature_type = ir_str_to_type(get_child(signature,TYPE_SPEC)->optional_value->value);
-            if(signature_type != nullptr){
+        if (auto signature = get_child(f_output, VARIABLE_SIGNATURE)) {
+            llvm::Type *signature_type = ir_str_to_type(get_child(signature, TYPE_SPEC)->optional_value->value);
+            if (signature_type != nullptr) {
                 return_type = signature_type;
             }
         }
@@ -87,7 +84,7 @@ llvm::Function * func_to_ir(std::shared_ptr<ast_node> &func_node){
     auto arguments_type = get_func_args_types(func_node);
 
 #ifdef DEBUG_MODE
-    std::cout << "[DEBUG] Generating IR for " << func_name << " with " << arguments_type.size() << " arguments_type\n";
+    std::cout << "[DEBUG] Generating IR for " << func_name << " with " << arguments_type.size() << " arguments\n";
 #endif
 
     llvm::FunctionType *FT = llvm::FunctionType::get(return_type, arguments_type, false);
@@ -95,29 +92,85 @@ llvm::Function * func_to_ir(std::shared_ptr<ast_node> &func_node){
     return F;
 }
 
-void node_to_ir(std::shared_ptr<ast_node> &node){
-    switch (node->node_type){
+llvm::Value *var_to_ir(std::shared_ptr<ast_node> &var_node) {
+    std::string var_name = get_child(var_node, AST_JUST_TEXT)->optional_value->value;
+    llvm::Value *V = named_values[var_name];
+    if (!V) {
+        std::cout << "If you see this - something is terribly wrong\n";
+    }
+    return V;
+
+}
+
+llvm::Value *num_to_ir(std::shared_ptr<ast_node> &var_node) {
+    float val = 0;
+    switch (var_node->node_type) {
+        case VAL_INT:
+            val = (int) std::stoi(var_node->optional_value->value);
+            return llvm::ConstantFP::get(*llvm_context, llvm::APFloat(val));
+        case VAL_FP:
+            val = std::stof(var_node->optional_value->value);
+            return llvm::ConstantFP::get(*llvm_context, llvm::APFloat(val));
+    }
+}
+
+llvm::Value *expr_to_ir(std::shared_ptr<ast_node> &expr_node) {
+    if (expr_node->children.size() == 2) {
+        llvm::Value *LHS = expr_to_ir(expr_node->children[0]);
+        llvm::Value *RHS = expr_to_ir(expr_node->children[1]);
+
+        switch (expr_node->node_type) {
+            case ADD:
+                return llvm_builder->CreateFAdd(LHS, RHS, "addtmp");
+            case SUB:
+                return llvm_builder->CreateFSub(LHS, RHS, "subtmp");
+            case MUL:
+                return llvm_builder->CreateFMul(LHS, RHS, "multmp");
+            case DIV:
+                return llvm_builder->CreateFDiv(LHS, RHS, "divtmp");
+            case COMP_GREATER:
+                return llvm_builder->CreateUIToFP(LHS, llvm::Type::getDoubleTy(*llvm_context), "addtmp");
+        }
+    }
+    else return num_to_ir(expr_node);
+}
+
+void node_to_ir(std::shared_ptr<ast_node> &node) {
+    switch (node->node_type) {
         case F_DECL:
             func_to_ir(node);
             break;     // Function declaration
 
-        case VAR_DECL: break;   // Global variables
-        case CONST_DECL: break; // Global variables
-        case ARR_DECL: break;   // Global variables
-        case STRUCT_DECL: break;// Global variables
+        case VAR_DECL:
+            var_to_ir(node);
+            break;   // Global variables
+        case CONST_DECL:
+            break; // Global variables
+        case ARR_DECL:
+            break;   // Global variables
+        case STRUCT_DECL:
+            break;// Global variables
+
+        case ADD:
+        case SUB:
+        case DIV:
+        case MUL:
+        case COMP_GREATER:
+            expr_to_ir(node);
+
 
     }
 }
 
 
-void generate_ir(std::shared_ptr<ast_node> &root){
+void generate_ir(std::shared_ptr<ast_node> &root) {
     initialize_ir();
-    for(auto child: root->children){
+    for (auto child: root->children) {
         node_to_ir(child);
     }
 
 #ifdef DEBUG_MODE
-    llvm_module->print(llvm::errs(), nullptr);
+    llvm_module->print(llvm::errs(), nullptr, false, true);
 #endif
 }
 
